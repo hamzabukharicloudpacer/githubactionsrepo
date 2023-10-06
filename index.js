c7d3db3a41723c1effb13636e47e2fca03e5c312
hamza <hamza.bukhari@clouddpacer.com>
2023-10-06 21:10:28 +0500

    2 commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Github Actions" }');
+    res.send('{ "response": "Hello From Github Actions - Hamza Bukhari" }');
 });
 
 app.get('/will', function (req, res) {
